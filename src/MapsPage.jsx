a2faf9d43f18a5d8ebcfac55d3e12e279b392eb9
Owen Black <owenblack@Owens-MacBook-Air.local>
2018-04-12 16:26:09 -0600

    Built map page fetch function

--- src/MapsPage.jsx
+++ src/MapsPage.jsx
@@ -3,13 +3,38 @@ import Header from './Header.jsx';
 import MapNav from './MapNav.jsx';
 
 class MapsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mapData: []
+    }
+  }
+ 
+  getMaps() {
+    const url = "https://g-events-api.herokuapp.com/maps";
+    let dataGrab = (response) => {
+      this.setState({mapData: response});
+      console.log(this.state.mapData);
+      
+    };
+    return fetch(url)
+          .then(response => response.json())
+          .then(dataGrab)
+          .catch()
+  }
 
+  componentWillMount() {
+    this.getMaps();
+  }
 
   render() {
     return (
       <div>
         <Header />
         <MapNav />
+        <div className="map-container">
+        {/* <img src= { this.state.mapData.LL[1]} alt=""/> */}
+        </div>
       </div>
     )
   }
