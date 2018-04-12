811256f4e370b34e32a9089c523525ad52facfb7
Vision-X <michael.s.marlow@gmail.com>
2018-04-12 17:38:57 -0600

    add static image to MapsPage component, adjusts styles to reflect map image

--- src/MapsPage.jsx
+++ src/MapsPage.jsx
@@ -9,13 +9,13 @@ class MapsPage extends Component {
       mapData: []
     }
   }
- 
+
   getMaps() {
     const url = "https://g-events-api.herokuapp.com/maps";
     let dataGrab = (response) => {
       this.setState({mapData: response});
       console.log(this.state.mapData);
-      
+      console.log(this.state.mapData[0]['LL'].portraitMapUrl);
     };
     return fetch(url)
           .then(response => response.json())
@@ -33,7 +33,7 @@ class MapsPage extends Component {
         <Header />
         <MapNav />
         <div className="map-container">
-        {/* <img src= { this.state.mapData.LL[1]} alt=""/> */}
+          <img src="http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorPortraitMap.png" alt=""/>
         </div>
       </div>
     )
