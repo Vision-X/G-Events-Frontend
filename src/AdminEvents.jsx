39591e77378e2a218b7f1cf8a768839f2d3865cb
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-04-13 15:32:25 -0600

    update form and events styles, move assests folder to public folder, resize galvanize_sign.jpg

--- src/AdminEvents.jsx
+++ src/AdminEvents.jsx
@@ -4,76 +4,80 @@ import Event from './Event.jsx';
 import EventForm from './EventForm';
 
 class AdminEvents extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
     }
-  }
 
-  getEvents() {
-    const url = "https://g-events-api.herokuapp.com/events";
-    let dataGrab = (response) => {
-      this.setState({data: response});
-    };
-    return fetch(url)
-          .then(response => response.json())
-          .then(dataGrab)
-          .catch()
-  }
+    getEvents() {
+        const url = "https://g-events-api.herokuapp.com/events";
+        let dataGrab = (response) => {
+            this.setState({ data: response });
+        };
+        return fetch(url)
+            .then(response => response.json())
+            .then(dataGrab)
+            .catch()
+    }
 
-  componentWillMount() {
-    this.getEvents();
-  }
+    componentWillMount() {
+        this.getEvents();
+    }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <EventForm />
-            {this.state.data.map(eventInfo => {
-                return (    
-                    <div className="event-detail-card content event-form">
-                        <div className="grid grid--gutters title-block">
-                            <div className="event-detail-block">
-                                <form id="event-input" onSubmit={(e) => this.onSubmit(e)}>
-                                    <div>
-                                        <label>Month:</label>
-                                        <input type="text" defaultValue={eventInfo.month} />
-                                        <label>Day:</label>
-                                        <input type="text" defaultValue={eventInfo.day}/>
-                                   
-                                    </div>
-                                    <div>
-                                        <label>Event Title:</label>
-                                        <input type="text" name="Event Title" defaultValue={eventInfo.title} />
-                                    </div>
-                                    <label>Time:</label>
-                                    <input type="text" name="time" defaultValue={eventInfo.time} />
-                                    <hr></hr>
-                                    <label>Location:</label>
-                                    <input type="text" defaultValue={eventInfo.location} />
+    render() {
+        return (
+            <div>
+                <Header />
+                <div>
+                    <h1>Events Admin Panel</h1>
+                </div>
+                <EventForm />
+                {this.state.data.map(eventInfo => {
+                    return (
+                        <div className="event-detail-card content event-form">
+                            <div className="grid grid--gutters title-block">
+                                <div className="event-detail-block">
+                                    <form id="event-input" onSubmit={(e) => this.onSubmit(e)}>
+                                        <div>
+                                            <label>Month:</label>
+                                            <input type="text" defaultValue={eventInfo.month} />
+                                            <label>Day:</label>
+                                            <input type="text" defaultValue={eventInfo.day} />
 
-                                    <label>Address:</label>
-                                    <input type="text" defaultValue="1644 Platte Street, Denver, CO 80202"/>
+                                        </div>
+                                        <div>
+                                            <label>Event Title:</label>
+                                            <input type="text" name="Event Title" defaultValue={eventInfo.title} />
+                                        </div>
+                                        <label>Time:</label>
+                                        <input type="text" name="time" defaultValue={eventInfo.time} />
+                                        <hr></hr>
+                                        <label>Location:</label>
+                                        <input type="text" defaultValue={eventInfo.location} />
 
-                                    <label>Room:</label>
-                                    <input type="text" name="room" />
-                                    <hr></hr>
-                                    <label>Event Description:</label>
-                                    <textarea type="text" name="Event Description" defaultValue={eventInfo.description} />
-                                    <input type="submit" value="Update Event"/>
-                                    <input type="submit" value="Delete Event"/>
-                                </form>
+                                        <label>Address:</label>
+                                        <input type="text" defaultValue="1644 Platte Street, Denver, CO 80202" />
+
+                                        <label>Room:</label>
+                                        <input type="text" name="room" />
+                                        <hr></hr>
+                                        <label>Event Description:</label>
+                                        <textarea type="text" name="Event Description" defaultValue={eventInfo.description} />
+                                        <input type="submit" value="Update Event" />
+                                        <input type="submit" value="Delete Event" />
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    )}
+                    )
+                }
                 )}
                 }
             </div>
-    )
-  }
+        )
+    }
 };
 
 export default AdminEvents;
--- src/EventsPage.jsx
+++ src/EventsPage.jsx
@@ -30,46 +30,44 @@ class EventsPage extends Component {
     return (
       <div>
         <Header />
+        <div className="events-info">
+          <img className="event-pic" src="./assets/galvanize_sign.jpg" alt="galvanize sign" />
+        </div>
         {this.state.data.map(eventInfo => {
           return (
-            <div>
-              <div>
-                <img src="./assets/galvanize_sign.jpg" alt="galvanize sign" />
+            <div className="event-detail-card content">
+              <div className="grid grid--gutters title-block">
+                <div className="event-detail-block">
+                  <div className="box">
+                    <h4 className="month">{eventInfo.month}</h4>
+                    <h6 className="day">{eventInfo.day}</h6>
+                  </div>
+                </div>
+                <div className="grid-cell">
+                  <h1 className="title">{eventInfo.title}</h1>
+                  <p className="time">
+                    {eventInfo.time}
+                  </p>
+                </div>
               </div>
-              <div className="event-detail-card content">
-                <div className="grid grid--gutters title-block">
-                  <div className="event-detail-block">
-                    <div className="box">
-                      <h4 className="month">{eventInfo.month}</h4>
-                      <h6 className="day">{eventInfo.day}</h6>
-                    </div>
-                  </div>
-                  <div className="grid-cell">
-                    <h1 className="title">{eventInfo.title}</h1>
-                    <p className="time">
-                      {eventInfo.time}
-                    </p>
-                  </div>
+              <hr></hr>
+              <div className="grid grid--gutters location-block">
+                <div className="grid-cell">
+                  <p className="address">
+                    <a href="#">
+                      {eventInfo.location}
+                    </a>
+                    <p>1644 Platte Street</p>
+                    <p>Denver, CO 80202</p>
+                  </p>
                 </div>
-                <hr></hr>
-                <div className="grid grid--gutters location-block">
-                  <div className="grid-cell">
-                    <p className="address">
-                      <a href="#">
-                        {eventInfo.location}
-                      </a>
-                      <p>1644 Platte Street</p>
-                      <p>Denver, CO 80202</p>
-                    </p>
-                  </div>
-                </div>
-                <hr className="short"></hr>
-                <div className="grid grid--gutters detail-block">
-                  <div className="grid-cell">
-                    <p className="details">
-                      {eventInfo.description}
-                    </p>
-                  </div>
+              </div>
+              <hr className="short"></hr>
+              <div className="grid grid--gutters detail-block">
+                <div className="grid-cell">
+                  <p className="details">
+                    {eventInfo.description}
+                  </p>
                 </div>
               </div>
             </div>
