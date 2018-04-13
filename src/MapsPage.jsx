9bcdf2c4cc11acc2c4baea84923217c90e5c7442
Vision-X <michael.s.marlow@gmail.com>
2018-04-13 14:44:47 -0600

    remove MapNav component, replace with nav button code inside MapPage, add level btn functionality on MapPage

--- src/MapsPage.jsx
+++ src/MapsPage.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
-import MapNav from './MapNav.jsx';
 
 class MapsPage extends Component {
   constructor() {
     super();
     this.state = {
       mapData: [],
-      LLUrl: ''
+      Levels: {
+      LLUrl: '',
+      L3Url: '',
+      L4Url: '',
+      mapImg: ''
+      }
     }
+    this._onClick = this._onClick.bind(this);
   }
 
   getMaps() {
@@ -16,8 +21,9 @@ class MapsPage extends Component {
     let dataGrab = (response) => {
       this.setState({mapData: response});
       console.log(this.state.mapData);
-      this.setState({fknUrl: response[0]['LL'].portraitMapUrl})
-      console.log(this.state.fknUrl, "fknurl");
+      this.setState({LLUrl: response[0]['LL'].portraitMapUrl})
+      this.setState({L3Url: response[0]['L3'].portraitMapUrl})
+      this.setState({L4Url: response[0]['L4'].portraitMapUrl})
 
       {/*    <img src={state.mapData[0]['LL'].portraitMapUrl} alt="" /> */}
 
@@ -28,17 +34,38 @@ class MapsPage extends Component {
           .catch()
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getMaps();
+  }
+
+  _onClick(event) {
+    if (event.target.type === 'submit') {
+      if (event.target.textContent === 'LL') {
+        console.log("lower level");
+        this.setState({mapImage: this.state.LLUrl})
+      } else if (event.target.textContent === 'L3') {
+        console.log("level three");
+        this.setState({mapImage: this.state.L3Url})
+      } else {
+        console.log("level four");
+        console.log(this.state.L4Url);
+        this.setState({mapImage: this.state.L4Url})
+        // return (<img src={this.props.data['L4Url']})
+      }
+    }
   }
 
   render() {
     return (
       <div>
         <Header />
-        <MapNav />
+        <div className="btn-group">
+          <button onClick={this._onClick} className="basic-button btn btn-secondary map-button">LL</button>
+          <button onClick={this._onClick} className="basic-button btn btn-secondary map-button">L3</button>
+          <button onClick={this._onClick} className="basic-button btn btn-secondary map-button">L4</button>
+        </div>
         <div className="map-container">
-          <img className="img-fluid" src={this.state.fknUrl} alt="" />
+          <img className="img-fluid" src={this.state.mapImage} alt="" />
         </div>
       </div>
     )
