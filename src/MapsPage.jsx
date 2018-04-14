1f5c9031a614e10ed303ccc61dbbe5721c8e2564
jasenmichael <jasen@ready-builder.com>
2018-04-14 17:18:19 -0600

    major refactor to maps ui, see waffle.io

--- src/MapsPage.jsx
+++ src/MapsPage.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import ToggleDisplay from 'react-toggle-display';
+
+const url = "https://g-events-api.herokuapp.com/rooms";
 
 class MapsPage extends Component {
   constructor() {
@@ -16,7 +19,6 @@ class MapsPage extends Component {
   };
 
   getMaps() {
-    const url = "https://g-events-api.herokuapp.com/rooms";
     let dataGrab = (response) => {
       this.setState({mapData: response});
       this.setState({LLUrl: response.rooms[0].roomMapPortraitUrl});
@@ -35,28 +37,49 @@ class MapsPage extends Component {
 
   _onClick(event) {
     if (event.target.type === 'submit') {
-      if (event.target.textContent === 'LL') {
+      if (event.target.textContent === 'Lower Level') {
         this.setState({mapImg: this.state.LLUrl});
-      } else if (event.target.textContent === 'L3') {
+      } else if (event.target.textContent === '3rd Floor') {
         this.setState({mapImg: this.state.L3Url});
       } else {
         this.setState({mapImg: this.state.L4Url});
       }
     }
+
   };
 
   render() {
     return (
       <div>
-        <div className="button-group map-nav fixed-top">
-          <a href="#bottom" className="basic-button btn btn-secondary map-button"><button onClick={this._onClick} className="basic-button btn btn-secondary map-button">LL</button></a>
-          <a href="#bottom" className="basic-button btn btn-secondary map-button"><button onClick={this._onClick} className="basic-button btn btn-secondary map-button">L3</button></a>
-          <a href="#bottom" className="basic-button btn btn-secondary map-button"><button onClick={this._onClick} className="basic-button btn btn-secondary map-button">L4</button></a>
-        </div>
+
+        <ToggleDisplay show={!this.state.mapImg}>
+          <div>
+            <div className="landing-btns mx-auto">
+              <a href="#bottom"><button onClick={this._onClick} className="landing btn-primary basic-button">4th Floor</button></a>
+            </div>
+            <div className="landing-btns mx-auto">
+              <a href="#bottom"><button onClick={this._onClick} className="landing btn-primary basic-button">3rd Floor</button></a>
+            </div>
+            <div className="landing-btns mx-auto">
+              <a href="#bottom"><button onClick={this._onClick} className="landing btn-primary basic-button">Lower Level</button></a>
+            </div>
+          </div>
+        </ToggleDisplay>
+
+        <ToggleDisplay show={!this.state.mapImg == ''}>
+          <div className="btn-group fixed-bottom}">
+            <a href="#bottom"><button onClick={this._onClick} className="basic-button selected btn btn-secondary map-button">Lower Level</button></a>
+            <a href="#bottom"><button onClick={this._onClick} className="basic-button btn btn-secondary map-button">3rd Floor</button></a>
+            <a href="#bottom"><button onClick={this._onClick} className="basic-button btn btn-secondary map-button">4th Floor</button></a>
+          </div>
+        </ToggleDisplay>
+
+
         <div className="map-container">
           <img className="img-fluid" src={this.state.mapImg} alt="" />
         </div>
-        <a id="bottom"></a>
+
+        <span id="bottom"></span>
       </div>
     )
   }
