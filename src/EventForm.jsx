1a56ca11ee547d5323c26907a5119f5b9e0a81e7
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-04-15 15:57:32 -0600

    clean up form component

--- src/EventForm.jsx
+++ src/EventForm.jsx
@@ -25,11 +25,9 @@ class InputForm extends Component {
         const message = 'Your event was submitted!';
         this.setState({
             success: [message],
-            EventInput: [EventInfo],            
+            EventInput: [EventInfo],
         });
-        
     }
-
     render() {
         return (
             <div className="event-detail-card content event-form">
@@ -37,16 +35,12 @@ class InputForm extends Component {
                     <div className="event-detail-block">
                         <form id="event-input" onSubmit={(e) => this.onSubmit(e)}>
                             <h1>Add Event Here: </h1>
-                            <div>
-                                <label>Month:</label>
-                                <input ref={(input) => this.month = input} type="text" name="Month" />
-                                <label>Day:</label>
-                                <input ref={(input) => this.day = input} type="text" Name="Day" />
-                            </div>
-                            <div>
-                                <label>Event Title:</label>
-                                <input ref={(input) => this.title = input} type="text" name="Event Title" />
-                            </div>
+                            <label>Month:</label>
+                            <input ref={(input) => this.month = input} type="text" name="Month" />
+                            <label>Day:</label>
+                            <input ref={(input) => this.day = input} type="text" Name="Day" />
+                            <label>Event Title:</label>
+                            <input ref={(input) => this.title = input} type="text" name="Event Title" />
                             <label>Time:</label>
                             <input ref={(input) => this.time = input} type="text" name="time" />
                             <hr></hr>
