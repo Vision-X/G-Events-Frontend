63aa1af41fd6a24bd4420df5c91e7a20d86a2f5a
jasenmichael <jasen@ready-builder.com>
2018-04-15 00:03:05 -0600

    removed unused classes, changed logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,11 +7,11 @@ class Header extends Component {
     return (
       <header className="fixed-top">
         <nav className="navbar">
-          <nav className="navbar navbar-inverse navbar-fixed-top">
+          <nav className="navbar navbar-fixed-top">
             <div className="container-fluid">
               <div className="navbar-header">
-                <a className="navbar-brand mx-auto mx-auto gorange">
-                  <img src="http://jasenmichael.com/img/galvanize-logo.png" alt="gSchool logo" />
+                <a className="navbar-brand mx-auto">
+                  <img class="img-fluid" src="http://jasenmichael.com/img/galvanize-logo-2.png" alt="gSchool logo" />
                 </a>
               </div>
               <h3>Events Locator</h3>
